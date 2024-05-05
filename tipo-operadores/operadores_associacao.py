997deeed70736f6974803140889495f8becd94e7
ALEXANDRE MIELI POLETINI <alminidev@gmail.com>
2024-05-05 01:05:01 -0300

    tipo-operadores

---
+++ tipo-operadores/operadores_associacao.py
@@ -0,0 +1,12 @@
+# OPERADOR IN pra saber se algo está na sequência
+# NOT IN Pra verivicar se algo não está na sequência
+
+frutas = ["limao", "uva"]
+curso = "Curso de python"
+
+print("laranja" not in frutas)
+print("laranja" in frutas)
+print("limao" in frutas)
+print("limao" not in frutas)
+print("Python" in curso)
+print("Python" not in curso)
---
+++ tipo-operadores/operadores_identidade.py
@@ -0,0 +1,7 @@
+# OPERADOR DE IDENTIDADE "IS" - "IS NOT"
+
+saldo = 1000
+limite = 1000
+
+print(saldo is limite)
+print(saldo is not limite)
